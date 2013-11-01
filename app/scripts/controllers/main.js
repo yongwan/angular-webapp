a43316d4c3356d979bdd16bb08bc8dfca454c7b3
YongWan Jo <yongwan.jo@aveva.com>
2013-11-01 18:16:36 +0900

    Apply twitter bootstrap for main page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,38 @@
 'use strict';
 
-angular.module('angularWebappApp')
+angular.module('angularWebApp')
   .controller('MainCtrl', function ($scope) {
+    $scope.site = {
+      title: '/* LIFE RUNS ON CODE */',
+      author: {
+        name: 'YongWan Jo',
+        github: 'yongwan',
+        twitter: 'yongwany',
+        facebook: 'yongwan.jo'
+      }
+    };
+
+    $scope.navs = [
+        {
+          url: '/archive',
+          title: 'Archive'
+        },
+        {
+          url: '/categories',
+          title: 'Categories'
+        },
+        {
+          url: '/tags',
+          title: 'Tags'
+        }
+    ];
+
+    $scope.page = {
+      title: 'Page Title',
+      tagline: 'Page Tagline'
+    };
+    
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',

e4f20ab718088d19387e6660c2fc9c220fdcd6c5
YongWan Jo <yongwan.jo@aveva.com>
2013-11-27 17:10:04 +0900

    Update bower components and apply codes to fit on angular 1.2.2

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,17 +1,23 @@
-'use strict';
+(function (angular) {
+    'use strict';
 
-angular.module('angularWebApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    angular.module('angularWebApp', [
+        'ngRoute',
+        'ngCookies',
+        'ngResource',
+        'ngSanitize'
+    ]).config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/posts/:year/:month/:day/:title', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }]);
+}(angular));
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,41 +1,43 @@
-'use strict';
+(function (angular) {
+    'use strict';
 
-angular.module('angularWebApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.site = {
-      title: '/* LIFE RUNS ON CODE */',
-      author: {
-        name: 'YongWan Jo',
-        github: 'yongwan',
-        twitter: 'yongwany',
-        facebook: 'yongwan.jo'
-      }
-    };
+    angular.module('angularWebApp')
+      .controller('MainCtrl', function ($scope) {
+          $scope.site = {
+              title: '/* LIFE RUNS ON CODE */',
+              author: {
+                  name: 'YongWan Jo',
+                  github: 'yongwan',
+                  twitter: 'yongwany',
+                  facebook: 'yongwan.jo'
+              }
+          };
 
-    $scope.navs = [
-        {
-          url: '/archive',
-          title: 'Archive'
-        },
-        {
-          url: '/categories',
-          title: 'Categories'
-        },
-        {
-          url: '/tags',
-          title: 'Tags'
-        }
-    ];
+          $scope.navs = [
+              {
+                  url: '/archive',
+                  title: 'Archive'
+              },
+              {
+                  url: '/categories',
+                  title: 'Categories'
+              },
+              {
+                  url: '/tags',
+                  title: 'Tags'
+              }
+          ];
 
-    $scope.page = {
-      title: 'Page Title',
-      tagline: 'Page Tagline'
-    };
-    
+          $scope.page = {
+              title: 'Page Title',
+              tagline: 'Page Tagline'
+          };
 
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+          $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+          ];
+      });
+}(angular));
